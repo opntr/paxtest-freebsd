aaa67479e89b45d48696776148f3b04915f5f668
Oliver Pinter <oliver.pntr@gmail.com>
2014-03-24 02:32:57 +0100

    change hard-coded linux interpreter to hard-coded freebsd
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

--- interp.c
+++ interp.c
@@ -1,5 +1,1 @@
-#ifdef __UCLIBC__
-const char __invoke_dynamic_linker__[] __attribute__ ((section (".interp"))) = "/lib/ld-uClibc.so.0";
-#else
-const char __invoke_dynamic_linker__[] __attribute__ ((section (".interp"))) = "/lib/ld-linux.so.2";
-#endif
+const char __invoke_dynamic_linker__[] __attribute__ ((section (".interp"))) = "/libexec/ld-elf.so.1";
